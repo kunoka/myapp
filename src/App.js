b6d3e5489a90154566579fee45c2eab41dfe69cb
Harry <hengwang@cn.ibm.com>
2019-02-28 07:33:40 +0800

    3-7 antd-moile

--- src/App.js
+++ src/App.js
@@ -1,28 +1,31 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component, Fragment} from 'react';
+import {Button} from 'antd-mobile';
+export default class App extends Component {
 
-class App extends Component {
   render() {
+    const boss = '赵云龙';
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+      <Fragment>
+      <Button type="primary">Submit</Button>
+      <div>园长{boss}</div>
+      <一营 老大='张大喵'></一营>
+      <一营2 老大='张大里'></一营2>
+      </Fragment>
+    )
   }
 }
 
-export default App;
+class 一营 extends Component {
+  render() {
+    return (
+        <div>一营长{this.props.老大}</div>
+    )
+  }
+}
+
+function 一营2(props){
+  return(
+  <div>一营长{props.老大}</div>
+  )
+
+}
