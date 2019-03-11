8b66133472364a9000e97ba200049aefca297a11
Harry <hengwang@cn.ibm.com>
2019-03-12 07:22:20 +0800

    6-8 登录注册-express注册功能实现 结束

--- server/index.js
+++ server/index.js
@@ -1,8 +1,12 @@
 const express = require('express');
-const userRouter = require('./user')
+const bodyParse = require('body-parser');
+const cookieParser = require('cookie-parser');
+
+const userRouter = require('./user');
 
 const app = express();
-
+app.use(cookieParser());
+app.use(bodyParse.json());
 app.use('/user', userRouter)
 app.listen(9093, function() {
 	console.log('Node app is running on port 9093');
