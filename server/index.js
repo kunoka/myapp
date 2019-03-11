857d930cf1a20d6d05e8eac97dedb1cb0f75b75c
Harry <hengwang@cn.ibm.com>
2019-03-12 07:08:51 +0800

    6-7 登录注册-数据库模型建立 结束

--- server/index.js
+++ server/index.js
@@ -1,13 +1,9 @@
 const express = require('express');
-const mongoose = require('mongoose');
-const userRouter = require('./userRouter');
+const userRouter = require('./user')
 
-const myapp = express();
-myapp.use('/user', userRouter);
-myapp.get('/', function(req, res) {
-	res.send('<h1>Hello Express 123</h1>' + new Date());
-});
+const app = express();
 
-myapp.listen(9093, function() {
+app.use('/user', userRouter)
+app.listen(9093, function() {
 	console.log('Node app is running on port 9093');
 })
--- server/model/index.js
+++ server/model/index.js
@@ -1,3 +1,31 @@
+const mongoose = require('mongoose');
 // 链接mongo 并且使用imooc这个集合
-const DB_URL = 'mongodb://127.0.0.1:27017/imooc';
+const DB_URL = 'mongodb://127.0.0.1:27017/imooc-chat';
 mongoose.connect(DB_URL);
+
+const models = {
+  user: {
+    'user': {'type': String, require: true},
+    'pwd': {'type': String, require: true},
+    'type': {'type': String, require: true},
+    'avatar': {'type': String},
+    'desc': {'type': String},
+    'title': {'type': String},
+
+    //如果是BOSS
+    'company': {'type': String},
+    'money': {'type': String},
+  },
+  chat: {
+
+  }
+}
+
+for (let m in models) {
+  mongoose.model(m, new mongoose.Schema(models[m]))
+}
+module.exports = {
+  getModel: function (name) {
+    return mongoose.model(name)
+  }
+}
