e7dd2b74672559ed38ee511564653c8a59dc0ddf
Harry <hengwang@cn.ibm.com>
2019-08-01 22:49:21 +0800

    10-2 socket.io前后端联通 结束

--- server/index.js
+++ server/index.js
@@ -1,13 +1,18 @@
 const express = require('express');
 const bodyParse = require('body-parser');
 const cookieParser = require('cookie-parser');
-
+const app = express();
+const http = require('http');
+const server = http.Server(app);
+const io = require('socket.io')(server);
+io.on('connection', function(socket) {
+	console.log('user login');
+})
 const userRouter = require('./user');
 
-const app = express();
 app.use(cookieParser());
 app.use(bodyParse.json());
 app.use('/user', userRouter)
-app.listen(9093, function() {
+server.listen(9093, function() {
 	console.log('Node app is running on port 9093');
 })
--- src/container/chat/index.js
+++ src/container/chat/index.js
@@ -1,6 +1,10 @@
 import React, {Component} from 'react';
-
+import io from 'socket.io-client';
 export default class Chat extends Component{
+  componentDidMount() {
+    const socket = io('ws://localhost:9093');
+  }
+
   render(){
     return(
       <h2>
